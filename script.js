aa8d28cfd1f4146285ef6cc2c32c789dcbca9855
sonal-gogavle07 <sonalgogavle@gmail.com>
2022-07-23 20:19:22 +0530

    code review

--- script.js
+++ script.js
@@ -7,20 +7,19 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show Loading
-function loading(){
+function showLoadingSpinner(){
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
-// Hide Loading
-function complete(){
+
+function removeLoadingSpinner(){
     loader.hidden = true;
     quoteContainer.hidden = false;
 }
 
 //Show new Quote
 function newQuote(){
-    loading();
+    showLoadingSpinner();
 
     //  Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
@@ -37,12 +36,12 @@ function newQuote(){
 
     // set Quote, hide loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 }
 
 //Get quotes from API
 async function getQuotes(){
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);// this means that this constant will not be populated until it has some data fetched from our API
